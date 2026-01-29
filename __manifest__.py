450434512a85e7f4a9a2c858b97afbcba1f9d6fb
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-01-29 10:55:13 +0500

    fix: update sale order view
    - Replace 'sale_order_template_views.xml' with 'sale_order_views.xml' in the module's view list

--- __manifest__.py
+++ __manifest__.py
@@ -28,7 +28,7 @@
         'views/account_move_views.xml',
         'views/boq_report_views.xml',
         'views/boq_line_views.xml',
-        'views/sale_order_template_views.xml',
+        'views/sale_order_views.xml', # [NEW] Added Sale Order View
     ],
     'installable': True,
     'application': True,
