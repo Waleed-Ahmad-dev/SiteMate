2243773d33a1ce814df6363d780d20ee1fecb2ac
Waleed-Ahmad-dev <itswaleedqureshi@gmail.com>
2026-01-29 10:56:58 +0500

    feat(sale_order): add BOQ management
    * Create new model for sale order with BOQ fields
    * Add action to create BOQ from confirmed sales order
    * Add action to view related BOQs
    * Remove deprecated sale order template model

---
+++ models/sale_order.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    boq_ids = fields.One2many('construction.boq', 'sale_order_id', string='BOQs')
+    boq_count = fields.Integer(compute='_compute_boq_count', string='BOQ Count')
+
+    @api.depends('boq_ids')
+    def _compute_boq_count(self):
+        for order in self:
+            order.boq_count = len(order.boq_ids)
+
+    def action_create_boq(self):
+        """
+        Action to create a Construction BOQ from a Confirmed Sales Order.
+        Opens the BOQ form with default values populated from the SO.
+        """
+        self.ensure_one()
+        
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Create BOQ'),
+            'res_model': 'construction.boq',
+            'view_mode': 'form',
+            'target': 'current',
+            'context': {
+                'default_sale_order_id': self.id,
+                'default_project_id': self.project_id.id if self.project_id else False,
+                'default_analytic_account_id': self.analytic_account_id.id if self.analytic_account_id else False,
+                'default_company_id': self.company_id.id,
+                'default_name': f"{self.name} - BOQ",
+            }
+        }
+
+    def action_view_boq(self):
+        """
+        Smart button action to view related BOQs.
+        """
+        self.ensure_one()
+        boqs = self.boq_ids
+        action = self.env['ir.actions.act_window']._for_xml_id('entrpryz_construction_boq.action_construction_boq')
+        
+        if len(boqs) > 1:
+            action['domain'] = [('id', 'in', boqs.ids)]
+        elif boqs:
+            action['views'] = [(self.env.ref('entrpryz_construction_boq.view_construction_boq_form').id, 'form')]
+            action['res_id'] = boqs.id
+        
+        # Pass default context for creating new BOQ from this view
+        action['context'] = {
+            'default_sale_order_id': self.id,
+            'default_project_id': self.project_id.id if self.project_id else False,
+        }
+        return action
